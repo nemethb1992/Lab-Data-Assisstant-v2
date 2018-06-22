167ce3e20a3f7f1ef6efe54805f5df916647c158
nemethb1992 <39947176+nemethb1992@users.noreply.github.com>
2018-06-22 13:24:50 +0200

    commit

--- LDAv2/LDAv2/Views/Panels/AddNewPanel.xaml.cs
+++ LDAv2/LDAv2/Views/Panels/AddNewPanel.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +26,11 @@ namespace LDAv2.Views.Panels
         {
             InitializeComponent();
             this.grid = grid;
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
